c31b459cd6a04d6017ac870825482227d48725f9
anavla777 <w.ananenkow@gmail.com>
2024-04-20 02:21:36 +0300

    Обновил xpath и добавил js для удаления баннеров

--- src/test/java/DemoqaFormTest.java
+++ src/test/java/DemoqaFormTest.java
@@ -17,6 +17,9 @@ public class DemoqaFormTest {
     @Test
     void fillPracticeForm() {
         open("/automation-practice-form");
+        executeJavaScript("$('footer').remove();");
+        executeJavaScript("$('#fixedban').remove();");
+
         $("#firstName").setValue("Jensen");
         $("#lastName").setValue("Huang");
         $("#userEmail").setValue("test@mail.com");
@@ -26,25 +29,30 @@ public class DemoqaFormTest {
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("1970");
         $(".react-datepicker__day--001").click();
+
         $("#subjectsInput").setValue("Physics").pressEnter();
         $("#subjectsInput").setValue("Computer Science").pressEnter();
+
         $(byText("Reading")).click();
         $(byText("Music")).click();
+
         $("#uploadPicture").uploadFromClasspath("selenide-logo-big.png");
+
         $("#currentAddress").setValue("Groove street 1");
         $("#react-select-3-input").setValue("Haryana").pressEnter();
         $("#react-select-4-input").setValue("Panipat").pressEnter();
-        $("#submit").click();
-        $x("//table/tbody/tr[1]/td[2]").shouldHave(text("Jensen Huang"));
-        $x("//table/tbody/tr[2]/td[2]").shouldHave(text("test@mail.com"));
-        $x("//table/tbody/tr[3]/td[2]").shouldHave(text("Male"));
-        $x("//table/tbody/tr[4]/td[2]").shouldHave(text("1111111111"));
-        $x("//table/tbody/tr[5]/td[2]").shouldHave(text("01 January,1970"));
-        $x("//table/tbody/tr[6]/td[2]").shouldHave(text("Physics, Computer Science"));
-        $x("//table/tbody/tr[7]/td[2]").shouldHave(text("Reading, Music"));
-        $x("//table/tbody/tr[8]/td[2]").shouldHave(text("selenide-logo-big.png"));
-        $x("//table/tbody/tr[9]/td[2]").shouldHave(text("Groove street 1"));
-        $x("//table/tbody/tr[10]/td[2]").shouldHave(text("Haryana Panipat"));
 
+        $("#submit").click();
+
+        $x("//td[contains(text(),'Student Name')]/../td[2]").shouldHave(text("Jensen Huang"));
+        $x("//td[contains(text(),'Student Email')]/../td[2]").shouldHave(text("test@mail.com"));
+        $x("//td[contains(text(),'Gender')]/../td[2]").shouldHave(text("Male"));
+        $x("//td[contains(text(),'Mobile')]/../td[2]").shouldHave(text("1111111111"));
+        $x("//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(text("01 January,1970"));
+        $x("//td[contains(text(),'Subjects')]/../td[2]").shouldHave(text("Physics, Computer Science"));
+        $x("//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(text("Reading, Music"));
+        $x("//td[contains(text(),'Picture')]/../td[2]").shouldHave(text("selenide-logo-big.png"));
+        $x("//td[contains(text(),'Address')]/../td[2]").shouldHave(text("Groove street 1"));
+        $x("//td[contains(text(),'State and City')]/../td[2]").shouldHave(text("Haryana Panipat"));
     }
 }
